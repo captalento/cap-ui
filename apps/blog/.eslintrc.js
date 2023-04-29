cebe5326495617bc25c07d6877f353fb03b6dfe2
Isaias Mendes <isaiaszmendes@gmail.com>
2023-04-28 23:51:56 -0300

    add contentful client

--- apps/blog/.eslintrc.js
+++ apps/blog/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: ["custom", "next"],
+  env: {
+    "CONTENTFUL_SPACE": true
+  }
 };
---
+++ apps/blog/datalayer/contentful/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from 'contentful';
+
+export const client = createClient({
+  space: process.env.CONTENTFUL_SPACE || '',
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || '',
+  environment: process.env.CONTENTFUL_ENVIRONMENT || ''
+})
