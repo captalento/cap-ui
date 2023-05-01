2c60ccddb73b798f1b8f7465ebca10e519b7ab63
Isaias Mendes <isaiaszmendes@gmail.com>
2023-04-30 23:47:02 -0300

    rn components

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,19 +1,12 @@
 import { Text, Heading, DSProvider, Card, Button } from '@captalento/design-system';
 import { useTheme } from '../hooks/useTheme'
 export default function Home() {
-  const { theme, toggleTheme} = useTheme()
+  const { theme, toggleTheme } = useTheme();
   return (
     <DSProvider theme={theme}>
       <section>
         <Heading>Captalento</Heading>
         <Text>Conteudo sobre Programação!</Text>
-        <Text size='sm'>Design Pattern</Text>
-        <Text size='md'>DDD</Text>
-        <Text size='lg'>large</Text>
-        <Text>DDD</Text>
-        <Card>Youtube</Card>
-        <Card>Instagram</Card>
-        <Card>Blog</Card>
         <Button onClick={toggleTheme}>Click</Button>
       </section>
     </DSProvider>
