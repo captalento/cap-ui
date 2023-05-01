15a2eb5525bfced72b4917e9b6384381a9f705e8
Isaias Mendes <isaiaszmendes@gmail.com>
2023-05-01 16:53:28 -0300

    add styles and props to heading

--- packages/design-system/src/atoms/heading/@Heading.tsx
+++ packages/design-system/src/atoms/heading/@Heading.tsx
@@ -1,10 +1,18 @@
 import { HTMLAttributes } from "react";
+import clsx from 'clsx';
 
 import * as styles from './heading.css';
-export type HeadingProps = HTMLAttributes<HTMLHeadingElement>;
 
-export function Heading({ children, ...props }: HeadingProps) {
+export type HeadingProps = HTMLAttributes<HTMLHeadingElement> & {
+  color?: 'primary' | 'secondary' | 'blue' | 'yellow' | 'red' | 'green' | 'orange';
+  size?: 'sm' | 'md' | 'lg';
+};
+
+export function Heading({ children, color = 'primary', size = 'md', ...props }: HeadingProps) {
+  const colorClass = styles[`${color}Class`];
+  const sizeClass = styles[`${size}Class`];
+
   return (
-    <h1 {...props} className={styles.headingClass}>{children}</h1>
+    <h1 {...props} className={clsx(styles.headingClass, colorClass, sizeClass)}>{children}</h1>
   )
 }
--- packages/design-system/src/atoms/heading/heading.css.ts
+++ packages/design-system/src/atoms/heading/heading.css.ts
@@ -1,3 +1,40 @@
 import { style } from '@vanilla-extract/css';
 import { vars } from '../../styles/theme.css'
 export const headingClass = style({ color: vars.theme.primary,	fontSize: "40px"})
+
+
+export const primaryClass = style({
+  color: vars.theme.primary,
+});
+
+export const secondaryClass = style({
+  color: vars.theme.secondary,
+});
+
+export const blueClass = style({
+  color: vars.colors.blue.blue100,
+});
+export const yellowClass = style({
+  color: vars.colors.yellow.yellow100,
+});
+export const orangeClass = style({
+  color: vars.colors.orange.orange100,
+});
+export const greenClass = style({
+  color: vars.colors.green.green100,
+});
+export const redClass = style({
+  color: vars.colors.red.red100,
+});
+
+export const lgClass = style({
+  fontSize: '40px',
+});
+
+export const mdClass = style({
+  fontSize: '30px',
+});
+
+export const smClass = style({
+  fontSize: '20px',
+});
