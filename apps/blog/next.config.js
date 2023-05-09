6ddab735ec0d0338e1b7c327a150fe0407075d10
Isaias Mendes <isaiaszmendes@gmail.com>
2023-05-08 23:48:06 -0300

    config

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,11 +1,10 @@
+/** @type {import('next').NextConfig} */
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [],
-  transpilePackages: ["ui", "@captalento/design-system"],
+  transpilePackages: ["@captalento/design-system"],
 };
 
 module.exports = withVanillaExtract(nextConfig);
