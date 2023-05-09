931db8a835bc2b4f463ed46ed78eccc3a44bdc6d
Isaias Mendes <isaiaszmendes@gmail.com>
2023-05-08 23:49:56 -0300

    add sprinkles

--- packages/design-system/src/styles/sprinkles.css.ts
+++ packages/design-system/src/styles/sprinkles.css.ts
@@ -1,20 +1,104 @@
-import { createSprinkles, defineProperties } from "@vanilla-extract/sprinkles";
-import { vars } from "./theme.css";
+import {
+  defineProperties,
+  createSprinkles
+} from '@vanilla-extract/sprinkles';
+import { colors } from '@captalento/design-tokens'
+
+const space = {
+  none: 0,
+  small: '4px',
+  medium: '8px',
+  large: '16px',
+  largest: '32px'
+  // etc.
+};
 
 const responsiveProperties = defineProperties({
   conditions: {
     mobile: {},
-    tablet: { "@media": "screen and (min-width: 768px)" },
-    desktop: { "@media": "screen and (min-width: 1024px)" }
+    tablet: { '@media': 'screen and (min-width: 768px)' },
+    desktop: { '@media': 'screen and (min-width: 1024px)' }
   },
-  defaultCondition: "mobile",
+  defaultCondition: 'mobile',
   properties: {
-    fontSize: vars.fontSizes,
-    lineHeight: vars.lineHeights
+    display: ['none', 'flex', 'block', 'inline'],
+    flexDirection: ['row', 'column'],
+    justifyContent: [
+      'stretch',
+      'flex-start',
+      'center',
+      'flex-end',
+      'space-around',
+      'space-between'
+    ],
+    alignItems: [
+      'stretch',
+      'flex-start',
+      'center',
+      'flex-end'
+    ],
+    paddingTop: space,
+    paddingBottom: space,
+    paddingLeft: space,
+    paddingRight: space,
+
+    marginTop: space,
+    marginBottom: space,
+    marginLeft: space,
+    marginRight: space
+    // etc.
   },
   shorthands: {
-    text: ["fontSize", "lineHeight"]
+    padding: [
+      'paddingTop',
+      'paddingBottom',
+      'paddingLeft',
+      'paddingRight'
+    ],
+    paddingX: ['paddingLeft', 'paddingRight'],
+    paddingY: ['paddingTop', 'paddingBottom'],
+    margin: [
+      'marginTop',
+      'marginBottom',
+      'marginLeft',
+      'marginRight'
+    ],
+    marginX: ['marginLeft', 'marginRight'],
+    marginY: ['marginTop', 'marginBottom'],
+    placeItems: ['justifyContent', 'alignItems']
   }
 });
 
-export const sprinkles = createSprinkles(responsiveProperties);
+const colorsMap = {
+   ...colors.blue,
+   ...colors.white,
+   ...colors.black,
+   ...colors.red,
+   ...colors.gray,
+   ...colors.orange,
+   ...colors.yellow,
+   ...colors.purple,
+   ...colors.green,
+  // etc.
+};
+
+const colorProperties = defineProperties({
+  conditions: {
+    lightMode: {},
+    darkMode: { '@media': '(prefers-color-scheme: dark)' }
+  },
+  defaultCondition: 'lightMode',
+  properties: {
+    color: colorsMap,
+    background: colorsMap
+    // etc.
+  }
+});
+
+export const sprinkles = createSprinkles(
+  responsiveProperties,
+  colorProperties
+);
+
+// It's a good idea to export the Sprinkles type too
+export type Sprinkles = Parameters<typeof sprinkles>[0];
