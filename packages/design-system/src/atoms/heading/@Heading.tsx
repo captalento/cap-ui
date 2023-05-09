76528d1dde4334159b9e1f581ab6ecc55c70acfd
Isaias Mendes <isaiaszmendes@gmail.com>
2023-05-08 23:49:07 -0300

    heading class

--- packages/design-system/src/atoms/heading/@Heading.tsx
+++ packages/design-system/src/atoms/heading/@Heading.tsx
@@ -6,13 +6,16 @@ import * as styles from './heading.css';
 export type HeadingProps = HTMLAttributes<HTMLHeadingElement> & {
   color?: 'primary' | 'secondary' | 'blue' | 'yellow' | 'red' | 'green' | 'orange';
   size?: 'sm' | 'md' | 'lg';
+  className?: string
 };
 
-export function Heading({ children, color = 'primary', size = 'md', ...props }: HeadingProps) {
+export function Heading({
+  children, color = 'primary', size = 'md', className, ...props
+}: HeadingProps) {
   const colorClass = styles[`${color}Class`];
   const sizeClass = styles[`${size}Class`];
 
   return (
-    <h1 {...props} className={clsx(styles.headingClass, colorClass, sizeClass)}>{children}</h1>
+    <h1 {...props} className={clsx(styles.headingClass, colorClass, sizeClass, className)}>{children}</h1>
   )
 }
